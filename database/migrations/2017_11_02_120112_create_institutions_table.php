14696b3cabd43718e10cb4848c3e45e3e0126c35
abhilashmandaliya <abhilashmandaliya@gmail.com>
2017-11-02 17:53:19 +0530

    migrated few tables

--- database/migrations/2017_11_02_120112_create_institutions_table.php
+++ database/migrations/2017_11_02_120112_create_institutions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateConsultancyBranchesTable extends Migration
+class CreateInstitutionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateConsultancyBranchesTable extends Migration
      */
     public function up()
     {
-        Schema::create('consultancy_branches', function (Blueprint $table) {
+        Schema::create('institutes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('contactNumber');
-            $table->string('branchAddress');
-            $table->integer('branchCity')->unsigned();
-            $table->foreign('branchCity')->references('id')->on('cities');
+            $table->string('instituteName');
+            $table->integer('categoryId')->references('id')->on('institute_categories');
             $table->boolean('isVisible');
             $table->timestamps();
         });
@@ -31,6 +29,6 @@ class CreateConsultancyBranchesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('consultancy_branches');
+        Schema::dropIfExists('institutes');
     }
 }
